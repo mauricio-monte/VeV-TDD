88abd0de833f292a297af78aaf638eca696e6528
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-08-20 10:10:07 -0300

    testPassageiroNulo() implementado

--- enock-airlines/src/tests/PassageiroTest.java
+++ enock-airlines/src/tests/PassageiroTest.java
@@ -1,4 +1,15 @@
 package tests;
+import main.Passageiro;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PassageiroTest {
+    private Passageiro passageiro;
+
+    @Test
+    public void testPassageiroNulo(){
+        assertEquals(null, this.passageiro);
+    }
+
 }
