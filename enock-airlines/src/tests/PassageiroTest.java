74756c27628deb3027dfca3ca43b0c3474fbe92a
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-08-20 10:21:50 -0300

    testeAdcPassageiro() criado

--- enock-airlines/src/tests/PassageiroTest.java
+++ enock-airlines/src/tests/PassageiroTest.java
@@ -3,13 +3,23 @@ import main.Passageiro;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PassageiroTest {
     private Passageiro passageiro;
 
     @Test
-    public void testPassageiroNulo(){
+    public void testePassageiroNulo(){
         assertEquals(null, this.passageiro);
+    }
+
+    @Test
+    public void testeAdcPassageiro(){
+        this.passageiro = new Passageiro("Jão", "jao@mail.com", "senha123", "4002-8922");
+        assertEquals("Jão", this.passageiro.getNome());
+        assertEquals("jao@mail.com", this.passageiro.getEmail());
+        assertTrue(this.passageiro.validaSenha("senha123"));
+        assertEquals("4002-8922", this.passageiro.getTel());
     }
 
 }
