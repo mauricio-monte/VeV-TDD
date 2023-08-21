3f754980a1df0f0409a6b4772868bcad62687cef
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-08-21 17:51:33 -0300

    testeFiltrarVoosPorOrigem() implementado com êxito

--- enock-airlines/src/main/VooRepository.java
+++ enock-airlines/src/main/VooRepository.java
@@ -47,4 +47,20 @@ public class VooRepository {
 
         return lista;
     }
+
+    public String getVooPorOrigem(String origem) {
+        String lista = "";
+
+        for (int i = 0; i < this.voos.size(); i++){
+            Voo voo = this.voos.get(i);
+            if (voo.getOrigem().equals(origem)) {
+                lista += String.format(
+                        "ID: %d | Origem: %s | Destino: %s | Data: %s | Horário: %s | Preço: R$ %.2f\n",
+                        i, voo.getOrigem(), voo.getDestino(), voo.getData(), voo.getHorario(), voo.getPreco()
+                );
+            }
+        }
+
+        return lista;
+    }
 }
