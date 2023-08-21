efe04c31e28b5d4666c813e4ba7f4da665dc00b2
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-08-21 18:07:20 -0300

    ReservaTeste iniciada

--- enock-airlines/src/tests/ReservaTeste.java
+++ enock-airlines/src/tests/ReservaTeste.java
@@ -1,5 +1,19 @@
-package tests;
+import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReservaTeste {
 
+    private Reserva reserva;
+
+    public void construirReservaVazia() {
+        this.reserva = new Reserva();
+    }
+
+    @AfterEach
+    public void destruir(){
+        this.reserva = null;
+    }
 }
